97e4413b98cba194d9e9565bbda42d262e0c54ce
Alex <osipenko.aleksey@gmail.com>
2020-10-23 16:21:41 +0300

    fix user post errors

--- src/model/base.entity.ts
+++ src/model/base.entity.ts
@@ -13,14 +13,8 @@ export abstract class BaseEntity {
   @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
   createDateTime: Date;
 
-  @Column({ type: 'varchar', length: 300 })
-  createdBy: string;
-
   @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
   lastChangedDateTime: Date;
-
-  @Column({ type: 'varchar', length: 300 })
-  lastChangedBy: string;
 
   @Column({ type: 'varchar', length: 300, nullable: true })
   internalComment: string | null;
--- src/model/user.entity.ts
+++ src/model/user.entity.ts
@@ -1,10 +1,12 @@
 import { Entity, Column, OneToMany } from 'typeorm';
 import { BaseEntity } from './base.entity';
 import { PostEntity } from './post.entity';
+import { ApiProperty } from '@nestjsx/crud/lib/crud';
 
 @Entity({ name: 'user' })
 export class UserEntity extends BaseEntity {
 
+  @ApiProperty()
   @Column({ type: 'varchar', length: 200 })
   name: string;
 
